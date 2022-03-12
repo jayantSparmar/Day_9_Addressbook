687d358bf302febeabffa3b6f30af94ffb9a1c98
jayant <jayantparmar420@gmail.com>
2022-03-13 00:21:47 +0530

    Added the welcome message

---
+++ src/com/bridgelaqbz/Address_book.java
@@ -0,0 +1,5 @@
+package com.bridgelaqbz;
+
+public class Address_book {
+    }
+
---
+++ src/com/bridgelaqbz/Address_book_main.java
@@ -0,0 +1,7 @@
+package com.bridgelaqbz;
+
+public class Address_book_main {
+    public static void main(String[] args) {
+        System.out.println("welcome to the address book program");
+    }
+}
