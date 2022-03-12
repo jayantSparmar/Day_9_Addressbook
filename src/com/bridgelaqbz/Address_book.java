417d1b8620065753ca08c837e97940b530119241
jayant <jayantparmar420@gmail.com>
2022-03-13 00:36:49 +0530

    Added Menu for AddressBook Management

--- src/com/bridgelaqbz/Address_book.java
+++
@@ -1,6 +0,0 @@
-package com.bridgelaqbz;
-
-public class Address_book {
-
-}
-
--- src/com/bridgelaqbz/Address_book_main.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelaqbz;
-
-public class Address_book_main {
-    public static void main(String[] args) {
-        System.out.println("welcome to the address book program");
-    }
-}
